90cacba5c47eb753153fdaeeec9aa7d89c78ff6f
Covali Andrei <krab4ek@gmail.com>
2022-08-21 12:14:24 +0300

    Add project files.

---
+++ SimpleGC/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    internal class Car
+    {
+        protected int CurrentSpeed { get; set; }
+        protected string? PetName { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string PetName, int CurrentSpeed)
+        {
+            this.PetName = PetName;
+            this.CurrentSpeed = CurrentSpeed;
+        }
+        public override string ToString()
+        
+           => $"{PetName} is going  {CurrentSpeed} MPH!";
+        
+    }
+}
