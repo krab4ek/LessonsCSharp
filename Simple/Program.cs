a9b78964dcba5de35ef15978fd2b75d6987bee65
Covali Andrei <krab4ek@gmail.com>
2022-09-23 22:08:12 +0300

    update

---
+++ Simple/Program.cs
@@ -0,0 +1,9 @@
+﻿for (int i = 1; i < 10; i++)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        Console.Write($"{j}:{i}*{j} = {i * j}\t");
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
--- SimpleGC/Program.cs
+++ SimpleGC/Program.cs
@@ -1,5 +1,24 @@
 ﻿using SimpleGC;
+using System.Globalization;
+
 Console.WriteLine("***** Simple GC *****");
 Car refToMyCar = new Car("Valera", 78);
 Console.WriteLine(refToMyCar.ToString());
+//do MakeACar();
+//while (true);
+MakeACar();
+static void MakeACar()
+{
+    Car myCar = new Car("Boris",56);
+    Console.WriteLine(myCar.ToString);
+}
 
+string str = "5,0";
+NumberFormatInfo number = new NumberFormatInfo()
+{
+    NumberDecimalSeparator = ",",
+};
+
+double d1 = double.Parse(str, number);
+Console.WriteLine(d1);
+Console.ReadLine();
