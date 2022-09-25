55ec4f2d5f68cc513195a4f3e10119808feabd80
Covali Andrei <krab4ek@gmail.com>
2022-09-25 10:14:05 +0300

    update

--- FunWithCollectionInitialization/Program.cs
+++ FunWithCollectionInitialization/Program.cs
@@ -3,16 +3,28 @@
 static void UseGenericsList()
 {
     List<Person> people = new List<Person>()
-{
+    {
     new Person ("Gomer","Simpson",45),
     new Person ("Marge","Simpson",42),
     new Person ("Bart", "Simpson", 17),
     new Person ("Lilu","Simpson",9)
 
-};
+    };
     Console.WriteLine("Items in list: {0}", people.Count);
 
+    foreach (Person person in people)
+        Console.WriteLine(person);
 
-}
+    Console.WriteLine("\n-> Inserting new person.");
+    people.Insert(2, new Person("Maggie", "Simpson", 3));
+
+    Console.WriteLine("Items in list: {0}", people.Count);
+    foreach (Person person in people)
+        Console.WriteLine(person);
+
+    Person[] arrayofPeople = people.ToArray();
+    foreach (Person person in arrayofPeople)
+        Console.WriteLine(person);
+};
 
 UseGenericsList();
